b1149a153e562cdf02d4e106819ff9d38b1ffd8a
Omega <omega@omegas-air.lan>
2021-07-20 21:17:32 +0800

    initial xcode project creation

---
+++ youtube-1D/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube-1D
+//
+//  Created by Omega on 14/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ youtube-1D/youtube_1DApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_1DApp.swift
+//  youtube-1D
+//
+//  Created by Omega on 14/7/21.
+//
+
+import SwiftUI
+
+@main
+struct youtube_1DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
